aae36d389783b550ae09558a27b7fe9a439b352e
junneyang <597092663@qq.com>
2017-04-28 18:32:16 +0800

    comment CustomUsernamePasswordAuthenticationFilter

--- common-web-starter-aaa/src/main/java/com/xcompany/xproject/common/web/starter/aaa/CustomUsernamePasswordAuthenticationFilter.java
+++ common-web-starter-aaa/src/main/java/com/xcompany/xproject/common/web/starter/aaa/CustomUsernamePasswordAuthenticationFilter.java
@@ -1,42 +1,42 @@
-package com.xcompany.xproject.common.web.starter.aaa;
-
-import java.io.IOException;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
-	@Override
-	public Authentication attemptAuthentication(HttpServletRequest request,
-			HttpServletResponse response) throws AuthenticationException {
-		// TODO Auto-generated method stub
-		ObjectMapper mapper = new ObjectMapper();
-		Map result = null; 
-		try {
-			result = mapper.readValue(request.getReader(), Map.class);
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-		String username = (String)result.get("username"); 
-		String password = (String)result.get("password");
-		
-		return super.attemptAuthentication(request, response);
-	}
-}
+//package com.xcompany.xproject.common.web.starter.aaa;
+//
+//import java.io.IOException;
+//import java.util.Map;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import com.fasterxml.jackson.core.JsonParseException;
+//import com.fasterxml.jackson.databind.JsonMappingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+//
+//	@Override
+//	public Authentication attemptAuthentication(HttpServletRequest request,
+//			HttpServletResponse response) throws AuthenticationException {
+//		// TODO Auto-generated method stub
+//		ObjectMapper mapper = new ObjectMapper();
+//		Map result = null; 
+//		try {
+//			result = mapper.readValue(request.getReader(), Map.class);
+//		} catch (JsonParseException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (JsonMappingException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} 
+//		String username = (String)result.get("username"); 
+//		String password = (String)result.get("password");
+//		
+//		return super.attemptAuthentication(request, response);
+//	}
+//}
